71388d86c2380d095b238608f73c9a1fd837099b
William Whitaker <billydwhitaker@gmail.com>
2021-05-13 17:52:36 -0400

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <h1 class="pieton-blue-fg">pomodoro</h1>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+/* Sample `apply` at-rules with Tailwind CSS
+.container {
+@apply min-h-screen flex justify-center items-center text-center mx-auto;
+}
+*/
+.container {
+  @apply min-h-screen flex justify-center items-center text-center mx-auto;
+}
+</style>
